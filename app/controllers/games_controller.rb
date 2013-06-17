11cd45cab967df1ea178a449f0b9b094477cd284
James Rottinger <JamesRottinger@gmail.com>
2013-06-17 01:26:01 -0400

    game form is done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,7 +6,29 @@ class GamesController < ApplicationController
 
 	def create
 		@game = Game.new(params[:game])
+		calc_and_update_ELO		
 		@game.save
 		redirect_to players_path
+	end
+
+	private
+
+	def calc_and_update_ELO
+
+		$player1 = Player.where(:name => @game.blue_offense).first
+		$player2 = Player.where(:name => @game.blue_defense).first
+
+		@game.blue_ELO = ($player1.ELO_rating + $player2.ELO_rating) / 2
+
+		$player1 = Player.where(:name => @game.red_offense).first
+		$player2 = Player.where(:name => @game.red_defense).first
+
+		@game.red_ELO = ($player1.ELO_rating + $player2.ELO_rating) / 2
+
+
+		Player.where(:name => @game.blue_offense).first.games << @game
+		Player.where(:name => @game.blue_defense).first.games << @game
+		Player.where(:name => @game.red_offense).first.games << @game
+		Player.where(:name => @game.red_defense).first.games << @game
 	end
 end
