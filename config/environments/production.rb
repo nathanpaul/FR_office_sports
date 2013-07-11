ea6f29d9de62d3fb1b9563e738a5e4e8b90a8678
James Rottinger <JamesRottinger@gmail.com>
2013-07-11 10:24:06 -0400

    add to production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ FROfficeSports::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
+  config.assets.precompile += ['static_pages.css']
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
